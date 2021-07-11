ce069cc3fdbdccd4459feed945428e57eac9df56
Dieng312 <ahmedieng2@gmail.com>
2021-07-11 17:45:28 +0200

    add attribut Difficulty to bloc

--- components/test_bloc.py
+++ components/test_bloc.py
@@ -5,4 +5,14 @@ setdlopenflags (RTLD_GLOBAL | 2)
 from bloc_component import Bloc
 b=Bloc()
 print (b.to_json())
-print(b.validationDifficultyBloc())
+print(" hash de bloc_0 : hash_0 = ", b.getHash())
+print(" Test validation du hash de bloc_0 : ", b.validationDifficultyBloc())
+
+b.setHash("1100123456789A123456789A123456789A123456789A123456789A123456789A")
+
+print(" hash de bloc_1 : hash_1 = ", b.getHash())
+print(" Test validation du hash de bloc_1 : ", b.validationDifficultyBloc())
+
+b.setHash("1100123456789A123456789A123456789A123456789A123456789A")
+print(" hash de bloc_2 : hash_2 = ", b.getHash())
+print(" Test validation du hash de bloc_2 : ", b.validationDifficultyBloc())
