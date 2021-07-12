a41942e41f7b32427e95ce54d293d7b2e9da3ad4
Dieng312 <ahmedieng2@gmail.com>
2021-07-12 23:53:52 +0200

    import sha256

--- bloc_component/bloc_component.cpp
+++ bloc_component/bloc_component.cpp
@@ -80,7 +80,7 @@ std::string Bloc::getPrevious_hash(){
 void Bloc::computeHash(){
     hash = "";
     py::object bloc_json = to_json();
-    hash = component_hachage::SHA256(bloc_json);
+    hash = sha256::SHA256(bloc_json);
 };
 
 
